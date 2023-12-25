4a6cd522e7f74404b41803517a7868374950923c
Muhammad Saim <muhammadsaim494@gmail.com>
2023-12-25 22:20:04 +0500

    css/js files, shortcode added

--- Mc_YT_Video_Downloader.php
+++ Mc_YT_Video_Downloader.php
@@ -1,6 +1,6 @@
 <?php
 
-use YouTube\YouTubeDownloader;
+// use YouTube\YouTubeDownloader;
 
 /**
  * Plugin Name: MC YouTube Video Downloader
@@ -40,14 +40,51 @@ if (!class_exists('Mc_YT_Video_Downloader')) {
             $this->yt_sample_video = '';
             $this->yt_thumbnail_url = 'https://i3.ytimg.com/vi/';
             $this->yt_video_id = '';
-            $this->youtube_dl = new YouTubeDownloader();
+            // $this->youtube_dl = new YouTubeDownloader();
             $this->youtube_api = 'https://youtube.com/get_video_info';
 
             // load composer's autoload
-            if (file_exists(trailingslashit(plugin_dir_path(__FILE__)) . 'vendor/autoload.php')) {
-                require_once trailingslashit(plugin_dir_path(__FILE__)) . 'vendor/autoload.php';
-            }
+            // if (file_exists(trailingslashit(plugin_dir_path(__FILE__)) . 'vendor/autoload.php')) {
+            //     require_once trailingslashit(plugin_dir_path(__FILE__)) . 'vendor/autoload.php';
+            // }
+
+            //shortcode
+            add_shortcode('Mc_YouTube_Video_Downloader', [$this, 'mc_shortcode_video_downloader']);
         }
+
+        /**
+         * create a shorcode for plugin
+         *
+         * @param array $atts
+         */
+        public function mc_shortcode_video_downloader($atts)
+        {
+            ob_start();
+            extract(
+                shortcode_atts(
+                    [
+                        'text' => 'Get',
+                    ],
+                    $atts,
+                    'Mc_YouTube_Video_Downloader'
+                )
+            );
+            $this->mc_youtube_render_downloader($text);
+            return ob_get_clean();
+        }
+
+        public function mc_youtube_render_downloader($text)
+        {
+            // css
+            wp_enqueue_style('mc-youtube-video-downloader-style', trailingslashit(plugin_dir_url(__FILE__)) . 'assets/css/style.css', [], null, 'all');
+            // js
+            wp_enqueue_script('mona-youtube-video-downloader-script', trailingslashit(plugin_dir_url(__FILE__)) . 'assets/js/script.js', ['jquery'], true);
+
+            ?>
+<h1 class="mc-youtube-video-downloader-h1">Mc YouTube Video Downloader
+    <?= $text ?>
+</h1>
+<?php }
 
         /**
          * extract youtube video id from the youtube vide URL
@@ -66,3 +103,4 @@ if (!class_exists('Mc_YT_Video_Downloader')) {
     }
     new Mc_YT_Video_Downloader();
 }// end if condition to check class already exists
+?>
